c63f1dd1c4e80c75d5de1a5d727eb587665d392a
David Blythe <49919035+writeDavid@users.noreply.github.com>
2026-09-08 13:27:12 -0700

    Update mail-dovecot.sh
    sieve changes for 2.4.5

--- setup/mail-dovecot.sh
+++ setup/mail-dovecot.sh
@@ -184,12 +184,24 @@ tools/editconf.py /etc/dovecot/conf.d/15-lda.conf \
 # * `sieve_dir`: Directory for :personal include scripts for the include extension. This
 # is also where the ManageSieve service stores the user's scripts.
 cat > /etc/dovecot/conf.d/99-local-sieve.conf << EOF;
-sieve_before = /etc/dovecot/sieve-spam.sieve
-sieve_before2 = $STORAGE_ROOT/mail/sieve/global_before
-sieve_after = $STORAGE_ROOT/mail/sieve/global_after
-sieve = $STORAGE_ROOT/mail/sieve/%d/%n.sieve
-sieve_dir = $STORAGE_ROOT/mail/sieve/%d/%n
-sieve_redirect_envelope_from = recipient
+sieve_script spam-global {
+    type = before
+    path = /etc/dovecot/sieve-spam.sieve
+}
+sieve_script spam-global-before {
+    type = before
+    path = $STORAGE_ROOT/mail/sieve/global_before
+}
+sieve_script spam-global-after {
+    type = after
+    path = $STORAGE_ROOT/mail/sieve/global_after
+}
+sieve_script personal {
+    type = personal
+    path = $STORAGE_ROOT/mail/sieve/%{user | domain }/%{user | username }
+    active_path = $STORAGE_ROOT/mail/sieve/%{user | domain }/%{user | username }.sieve
+    sieve_redirect_envelope_from = recipient
+}
 EOF
 
 # Copy the global sieve script into where we've told Dovecot to look for it. Then
