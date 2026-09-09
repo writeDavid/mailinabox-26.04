d6db157c27425020554c33e51140307c376185d3
David Blythe <49919035+writeDavid@users.noreply.github.com>
2026-09-09 12:38:19 -0700

    Update management.sh
    test for python packages

--- setup/management.sh
+++ setup/management.sh
@@ -14,12 +14,12 @@ echo "Installing Mail-in-a-Box system management daemon..."
 #
 # certbot installs EFF's certbot which we use to
 # provision free TLS certificates.
-apt_install duplicity python3-gnupg python3-pip virtualenv certbot rsync
+apt_install duplicity python3-gnupg python3-pip virtualenv python3-b2sdk python3-boto3 certbot rsync
 
 # b2sdk is used for backblaze backups.
 # boto3 is used for amazon aws backups.
 # Both are installed outside the pipenv, so they can be used by duplicity
-hide_output pip3 install --upgrade b2sdk boto3
+## TEST: hide_output pip3 install --upgrade b2sdk boto3
 
 # Create a virtualenv for the installation of Python 3 packages
 # used by the management daemon.
