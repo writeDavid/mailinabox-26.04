c4a4df481af68fe859ecbd5878095a6097da858d
David Blythe <49919035+writeDavid@users.noreply.github.com>
2026-09-10 07:51:14 -0700

    Update management.sh
    re-organize

--- setup/management.sh
+++ setup/management.sh
@@ -106,8 +106,8 @@ EOF
 chmod +x $inst_dir/start
 cp --remove-destination conf/mailinabox.service /lib/systemd/system/mailinabox.service # target was previously a symlink so remove it first
 ## hide_output systemctl link -f /lib/systemd/system/mailinabox.service
-hide_output systemctl daemon-reload
 hide_output systemctl enable /lib/systemd/system/mailinabox.service
+hide_output systemctl daemon-reload
 
 # Perform nightly tasks at 3am in system time: take a backup, run
 # status checks and email the administrator any changes.
